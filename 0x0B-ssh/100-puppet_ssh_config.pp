babeb3e8e9711893782f52674305b73a9dac862c
Florence Hanson <118609247+beautyscribbles@users.noreply.github.com>
2023-12-21 08:30:43 +0100

    Created 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet_script to_create ssh config_file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
